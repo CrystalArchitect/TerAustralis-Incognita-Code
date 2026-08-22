66049c08bf669832a8ea6b855e4757346c96a15b
Crystal Arena-Turner <teraustralis.incognita@gmail.com>
2026-08-22 11:35:37 +1000

    Header: name the three links that leave this origin.

--- vision/site/src/lib/components/Header.svelte
+++ vision/site/src/lib/components/Header.svelte
@@ -3,10 +3,16 @@
 
 <header class="site">
   <a class="wordmark" href="/">TerAustralis&nbsp;Incognita</a>
-  <nav aria-label="Sections">
-    <a href="https://proposal.teraustralis.com.au/">Sitting</a>
-    <a href="https://proposal.teraustralis.com.au/16-catch.html">Catch</a>
-    <a href="https://proposal.teraustralis.com.au/17-gate.html">Gate</a>
+  <nav aria-label="Primary">
+    <a href="https://proposal.teraustralis.com.au/"
+      >Sitting<span class="vh">, proposal site</span></a
+    >
+    <a href="https://proposal.teraustralis.com.au/16-catch.html"
+      >Catch<span class="vh">, proposal site</span></a
+    >
+    <a href="https://proposal.teraustralis.com.au/17-gate.html"
+      >Gate<span class="vh">, proposal site</span></a
+    >
     <a href="/clementine">Clementine</a>
     <a href="/docs">Archive</a>
   </nav>
